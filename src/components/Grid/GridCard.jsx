8bd42bc5e59a347420f63e6d59a0a2776d8d9e6c
Tsvetelin <kitanskitsvetelin@gmail.com>
2023-07-28 13:56:48 +0300

    put some data

--- src/components/Grid/GridCard.jsx
+++ src/components/Grid/GridCard.jsx
@@ -5,11 +5,11 @@ import Card from "react-bootstrap/Card";
 function GridCard(card) {
   return (
       <Card className="d-flex flex-row flex-wrap mb-5" style={{ width: "20rem" }}>
-        <Card.Img variant="top" src={card.img}></Card.Img>
+        <Card.Img variant="top" src={card.image}></Card.Img>
         <Card.Body className="d-flex flex-column">
           <div className="d-flex mb-2 justify-content-between">
           <Card.Title className="mb-0 font-weight-bold">{card.title}</Card.Title>
-          <Badge pill className="mb-2" bg="dark">$20</Badge>
+          <Badge pill className="mb-2" bg="dark">${card.price}</Badge>
           </div >
           <Card.Text className="text-secondary">{card.description}</Card.Text>
           <Button className="mt-auto font-weight-bold"variant="primary">Add to cart</Button>
